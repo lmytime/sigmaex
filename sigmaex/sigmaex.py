7d798376f08f9e555fb37221d986d8b230333d44
Mingyu Li <lmytime@hotmail.com>
2025-01-23 14:26:51 +0800

    Add chi2, fix nan case, add time to output filename, set default mode to 1 sigma

--- sigmaex/sigmaex.py
+++ sigmaex/sigmaex.py
@@ -15,9 +15,12 @@ from astropy.stats import sigma_clip
 from astropy.io import fits
 import tifffile as tiff
 import argparse
+from lmfit.models import GaussianModel
+from scipy.stats import norm
+import time
 
 class SigmaEx:
-    def __init__(self, data, sigma=3, mode="all", nbins=200, xmin=None, xmax=None, sample=1e7, path=None):
+    def __init__(self, data, sigma=3, mode="1sigma", nbins=200, xmin=None, xmax=None, sample=1e7, path=None):
         self.data = data
         self.sigma = sigma
         self.mode = mode
@@ -35,10 +38,10 @@ class SigmaEx:
             self.sample_flag = True
             self.data_flatten = np.random.choice(self.data_flatten, int(sample), replace=False)
 
-        self.mean = np.mean(self.data)
-        self.median = np.median(self.data)
-        self.std = np.std(self.data)
-        self.rms = np.sqrt(np.mean(self.data**2))
+        self.mean = np.nanmean(self.data)
+        self.median = np.nanmedian(self.data)
+        self.std = np.nanstd(self.data)
+        self.rms = np.sqrt(np.nanmean(self.data**2))
 
         data_flatten_sigma_clipped = sigma_clip(self.data_flatten, sigma=self.sigma, masked=False)
         self.data_sigma_clipped = data_flatten_sigma_clipped
@@ -64,11 +67,12 @@ class SigmaEx:
         return cls(data, path=path, **kwargs)
 
     def __1D_gaussian__(self, x, mu, sigma, A):
-        return A*np.exp(-0.5*np.power((x-mu)/sigma, 2))
+        return A/np.sqrt(2*np.pi)/sigma*np.exp(-0.5*np.power((x-mu)/sigma, 2))
 
-    def __sigma_ex__(self, nbins=1000, mode="all", xmin=None, xmax=None):
+    def __sigma_ex__(self, nbins=1000, mode="1sigma", xmin=None, xmax=None):
         n_all, bins = np.histogram(self.data_sigma_clipped, bins=nbins, density=True)
         bin_centers_all = (bins[:-1] + bins[1:])/2
+        bin_width = bins[1] - bins[0]
 
         if mode == "all":
             n = n_all
@@ -97,17 +101,38 @@ class SigmaEx:
         elif mode == "ge_std":
             n = n_all[bin_centers_all>self.sigma_clipped_std]
             bin_centers = bin_centers_all[bin_centers_all>self.sigma_clipped_std]
+        elif mode == "1sigma":
+            n = n_all[bin_centers_all<self.sigma_clipped_mean+self.sigma_clipped_std]
+            bin_centers = bin_centers_all[bin_centers_all<self.sigma_clipped_mean+self.sigma_clipped_std]
         elif mode == "custom":
             n = n_all[(bin_centers_all>xmin) & (bin_centers_all<xmax)]
             bin_centers = bin_centers_all[(bin_centers_all>xmin) & (bin_centers_all<xmax)]
         else:
-            raise ValueError("mode should be one of 'all', 'positive', 'negative', 'le_mean', 'ge_mean', 'le_median', 'ge_median', 'le_std', 'ge_std', 'custom'")
+            raise ValueError("mode should be one of 'all', 'positive', 'negative', 'le_mean', 'ge_mean', 'le_median', 'ge_median', 'le_std', 'ge_std', '1sigma', 'custom'")
 
-        popt, _ = curve_fit(self.__1D_gaussian__, bin_centers, n, p0=[self.sigma_clipped_mean, self.sigma_clipped_std, np.max(n)])
+        # Using curve_fit
+        # popt, _ = curve_fit(self.__1D_gaussian__, bin_centers, n, p0=[self.sigma_clipped_mean, self.sigma_clipped_std, np.max(n)])
+        # gaussian_fit_mu, gaussian_fit_sigma, gaussian_fit_A = popt
 
-        gaussian_fit_mu, gaussian_fit_sigma, gaussian_fit_A = popt
+        # Using lmfit
+        model = GaussianModel()
+        params = model.make_params(amplitude=np.max(n), center=self.sigma_clipped_mean, sigma=self.sigma_clipped_std)
+        result = model.fit(n, params, x=bin_centers)
+        self.fit_result = result
+        gaussian_fit_mu, gaussian_fit_sigma, gaussian_fit_A = result.best_values['center'], result.best_values['sigma'], result.best_values['amplitude']
+        # Estimate the reduced chi2
+        std = self.bi_std(result.best_fit, result.best_values['center'], result.best_values['sigma'], bin_centers, bin_width)
+        chi2 = np.sum(((result.best_fit - n)/std)**2)
+        redchi2 = chi2/(result.ndata-3)
+        self.redchi2 = redchi2
 
         return n_all, bin_centers_all, n, bins, bin_centers, gaussian_fit_mu, gaussian_fit_sigma, gaussian_fit_A
+
+
+    def bi_std(self, N, mu, sigma, x_cen, dx):
+        p = norm.cdf((x_cen + dx/2 - mu) / sigma) - norm.cdf((x_cen - dx/2 - mu) / sigma)
+        std = np.sqrt(N * p * (1-p))
+        return std
 
     def plot(self, ax=None, filename=None, **kwargs):
         if ax is None:
@@ -122,14 +147,14 @@ class SigmaEx:
                 label='Gaussian Fit\nμ = {:.2e}\nσ = {:.2e}'.format(self.gaussian_fit_mu, self.gaussian_fit_sigma, self.gaussian_fit_A))
 
         ax.axvline(self.gaussian_fit_mu, color='r', linestyle='-.')
-        ax.errorbar(self.gaussian_fit_mu, self.gaussian_fit_A/10, xerr=self.gaussian_fit_sigma, fmt='None', color='r',
+        ax.errorbar(self.gaussian_fit_mu, self.gaussian_fit_A/self.gaussian_fit_sigma/25, xerr=self.gaussian_fit_sigma, fmt='None', color='r',
                     elinewidth=2, capsize=5, capthick=2)
 
         ax.axvline(self.sigma_clipped_mean, color='k', linestyle='-.')
-        ax.errorbar(self.sigma_clipped_mean, self.gaussian_fit_A/9, xerr=self.sigma_clipped_std, fmt='None', color='k',
+        ax.errorbar(self.sigma_clipped_mean, self.gaussian_fit_A/self.gaussian_fit_sigma/22, xerr=self.sigma_clipped_std, fmt='None', color='k',
                     elinewidth=1, capsize=3, capthick=1)
 
-        ax.text(0.04, 0.96, f"nbins = {self.nbins}\nbin width = {self.bins[1]-self.bins[0]:.2e}", ha='left', va='top', transform=ax.transAxes)
+        ax.text(0.04, 0.96, f"nbins = {self.nbins}\nbin width = {self.bins[1]-self.bins[0]:.2e}\nReduced χ2 = {self.redchi2:.2f}", ha='left', va='top', transform=ax.transAxes)
         ax.set_xlabel('Pixel Value')
         ax.set_ylabel('Density')
 
@@ -195,6 +220,7 @@ class SigmaEx:
         gaussian_fit_sigma = self.__round__(self.gaussian_fit_sigma, n)
         bin_centers_min = self.__round__(self.bin_centers[0], n)
         bin_centers_max = self.__round__(self.bin_centers[-1], n)
+        redchi2 = f"{self.redchi2:.2f}"
 
         return f"------ σEx ------\
 \n{parameter_str}\n\
@@ -216,6 +242,7 @@ class SigmaEx:
 \nsigma_clipped_rms    = {sigma_clipped_rms}\
 \n\n:::::: σEx Fitting ::::::\
 \nX ~ N(μ, σ^2) --- fitting range: [{bin_centers_min}, {bin_centers_max}]\
+\nReduced χ^2 = {redchi2}\
 \nμ: gaussian_fit_mu    = {gaussian_fit_mu}\
 \nσ: gaussian_fit_sigma = {gaussian_fit_sigma}"
 
@@ -223,12 +250,12 @@ def main():
     parser = argparse.ArgumentParser(description='A python package for checking the gaussian histogram of data array.')
     parser.add_argument('data', type=str, help='Path to the data file.')
     parser.add_argument('-s', '--sigma', type=float, default=3, help='Sigma for sigma clipping.')
-    parser.add_argument('-m', '--mode', type=str, default='all', help='Mode for sigma_ex. One of "all", "positive", "negative", "le_mean", "ge_mean", "le_median", "ge_median", "le_std", "ge_std", "custom".')
+    parser.add_argument('-m', '--mode', type=str, default='1sigma', help='Mode for sigma_ex. One of "all", "positive", "negative", "le_mean", "ge_mean", "le_median", "ge_median", "le_std", "ge_std", "1sigma", "custom".')
     parser.add_argument('-n', '--nbins', type=int, default=200, help='Number of bins for the histogram.')
     parser.add_argument('--xmin', type=float, default=None, help='Minimum value for the custom mode.')
     parser.add_argument('--xmax', type=float, default=None, help='Maximum value for the custom mode.')
     parser.add_argument('--sample', type=float, default=1e7, help='Sample size for the data. If the data is too large, it will be sampled to this size.')
-    parser.add_argument('-o', '--output', type=str, default="sigmaex", help='Output filename for the plot.')
+    parser.add_argument('-o', '--output', type=str, default="sigmaex_"+time.strftime("%Y%m%d_%H%M%S", time.localtime()), help='Output filename for the plot.')
     args = parser.parse_args()
 
     if args.data.endswith('.fits'):
